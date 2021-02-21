5af20c1bd473378a227fcefdd16e3f0cffa38ae4
katerynadanko21 <katerynadanko21@gmail.com>
2021-02-21 16:59:41 +0200

    lesson-2

--- src/main/java/lesson4/exception/WrongSumException.java
+++
@@ -1,4 +0,0 @@
-package exception;
-
-public class WrongSumException extends Exception{
-}
--- src/main/java/lesson6/exceptions/StringFormatException.java
+++
@@ -1,6 +0,0 @@
-package lesson6.exceptions;
-
-import java.io.IOException;
-
-public class StringFormatException extends IOException {
-}
